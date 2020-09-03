2e3695f2377c06e3396f44d5c8002ebe0ad0b593
André Jr - NWT <ajjunior33@gmail.com>
2020-09-03 18:08:58 -0300

    Update - Initial Commit

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Dashboard(){
-    return(
-        <div className="container-fluid">
-            <h1>Hello,world!</h1>
-        </div>
-    )
+import "./style.css";
+
+function Dashboard() {
+  return (
+    <div className="wrapper">
+      <nav className="menu">
+        <ul>
+          <Link to="#">
+            <li>Página Inicial</li>
+          </Link>
+          <Link to="#">
+            <li>Meus carros</li>
+          </Link>
+          <Link to="#">
+            <li>Pedir novo rastreador</li>
+          </Link>
+        </ul>
+      </nav>
+
+      <section className="container-fluid">
+        <header>
+            <div className="logo">FLOTTUR</div>
+            <div className="logout">
+                
+            </div>
+        </header>
+
+        <section className="content-fluid">
+          <div className="box">
+            Meus Carros 5<footer>Verificar lista de carros</footer>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
 }
 
 export default Dashboard;
